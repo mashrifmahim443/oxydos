60d3c6276d948ea29567356b46498df897b01586
Mahim hassan <92907530+mashrifmahim443@users.noreply.github.com>
2025-02-28 11:52:00 +0600

    Update oxydos.py

--- oxydos.py
+++ oxydos.py
@@ -4,7 +4,7 @@ from langchain_openai import ChatOpenAI
 from langchain_core.output_parsers import StrOutputParser
 from langchain_core.prompts import ChatPromptTemplate
 import os
-
+#this is also updated in hugging face
 import os
 from dotenv import load_dotenv
 load_dotenv()
